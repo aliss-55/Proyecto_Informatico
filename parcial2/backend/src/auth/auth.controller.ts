4273f52ba49c34f22995c8191bccf0727145b48a
aliss-55 <alisson.tobar@uao.edu.co>
2023-10-07 16:23:06 -0500

    Adding auth service and controller

--- parcial2/backend/src/auth/auth.controller.ts
+++ parcial2/backend/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login-auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('login')
+    async loginUser(@Body() userObjectLogin: LoginDto) {
+        console.log(userObjectLogin);
+        const data = await this.authService.login(userObjectLogin);
+        //return this.authService.login(userObjectLogin);
+
+    }
+}
--- parcial2/backend/src/auth/auth.service.ts
+++ parcial2/backend/src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { Repository } from 'typeorm';
+import { LoginDto } from './dto/login-auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectRepository(User) private userRepository:
+        Repository<User>) { }
+
+    async login(userObjectLogin: LoginDto) {
+        const { email, password } = userObjectLogin;
+        const findUser = await this.userRepository.findOne({where: {email}});
+
+        if (!findUser) {
+            return false;
+        }
+        
+        const chechkPassword = await bcrypt.compare(password, findUser.password);
+        if (!chechkPassword){
+            return false;
+        }
+
+        return true;
+
+    }
+}
