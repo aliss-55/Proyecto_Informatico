0b1588ba5ff436e99c96e21ed698c183a2bab25b
aliss-55 <alisson.tobar@uao.edu.co>
2023-10-07 13:54:18 -0500

    Adding controller and imports-exports for the method

--- parcial2/backend/src/users/users.controller.ts
+++ parcial2/backend/src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService) { }
+
+    @Get()
+    getUsers(): Promise<User[]> {
+        return this.usersService.getUsers();
+    }
+
+    @Get(':email')
+    getUser(@Param('email') email: string): Promise<User[]> {
+        return this.usersService.getUsers();
+    }
+}
