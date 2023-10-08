855c285e1d53a0b337ebf2e51ea81f550aaa1338
aliss-55 <alisson.tobar@uao.edu.co>
2023-10-07 19:42:03 -0500

    Adding task entity

---
+++ parcial2/backend/src/tasks/task.entity.ts
@@ -0,0 +1,23 @@
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+
+enum TaskStatus {
+    Pending = 'PENDING',
+    In_Progress = 'IN_PROGRESS',
+    Done = 'DONE',
+}
+
+@Entity({ name: 'tasks' })
+export class Task {
+    
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column()
+    description: string
+
+    @Column()
+    status: string
+}
