cee91e7b5a9c508b02e4b21b5e09d68d977b7180
aliss-55 <alisson.tobar@uao.edu.co>
2023-10-07 23:12:43 -0500

    Adding the Database scripts

---
+++ parcial2/Database/Insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    `users` (user, email, password)
+VALUES
+    (
+        'user',
+        'user@gmail.com',
+        '$2b$10$8L3BQ.UBgk10BFLGwrlUn.4.Cw3jEA0wD9FWQdo9k3aG.oNa4K.US'
+    ),
+    (
+        'pablo',
+        'pablo.cardona@gmail.com',
+        '$2b$10$LH8NrqJ/UgUP/IGkV/ib3.2FNAHS.wooATj6kfLL45gs602EdsxCS'
+    );
