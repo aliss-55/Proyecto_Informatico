5c4e381fb1c3a8f135b9112532a9c08995c8fb30
aliss-55 <alisson.tobar@uao.edu.co>
2023-10-07 21:27:35 -0500

    Importing the entity to the Auth module

--- parcial2/backend/src/auth/auth.module.ts
+++ parcial2/backend/src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoginDto } from './dto/login-auth.dto';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forFeature([LoginDto]),
+  UsersModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
