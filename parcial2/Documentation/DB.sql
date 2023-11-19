6f4bedfe5551d8d91beb8945502e9e1a53116b9e
julian <julian_dav.velasquez@uao.edu.co>
2023-11-19 13:16:13 -0500

    change of the db schema wording for implementation in azure

--- parcial2/Documentation/DB.sql
+++ parcial2/Documentation/DB.sql
@@ -1,17 +1,27 @@
-CREATE DATABASE IF NOT EXISTS `askapp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'askapp')
+BEGIN
+    CREATE DATABASE askapp;
+END;
 
-USE `askapp`;
+USE askapp;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY,
-  `user` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(200) NOT NULL,
-  `password` VARCHAR(255) NOT NULL
-);
 
-CREATE TABLE `tasks` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `title` VARCHAR(255) NOT NULL,
-  `description` TEXT DEFAULT NULL,
-  `status` enum('Pendin','In_Progress','Done') NOT NULL
-);
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'users')
+BEGIN
+    CREATE TABLE users (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        [user] VARCHAR(50) NOT NULL,
+        email VARCHAR(200) NOT NULL,
+        [password] VARCHAR(255) NOT NULL
+    );
+END;
+
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tasks')
+BEGIN
+    CREATE TABLE tasks (
+        id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT NULL,
+        status VARCHAR(20) NOT NULL CHECK (status IN ('Pendin', 'In_Progress', 'Done'))
+    );
+END;
